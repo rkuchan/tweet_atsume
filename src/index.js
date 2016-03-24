a2c2d479e596293352c62aa2d568b20f806a8afa
Regan Kuchan <regankuchan@gmail.com>
2016-03-23 19:57:31 -0700

    Make request to Twitter API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('hi');
+// TODO: Move AJAX request to server
+
+import reqwest from 'reqwest'
+
+reqwest({
+  url: `https://api.twitter.com/1.1/search/tweets.json?q=${encodeURIComponent('#nekoatsume')}`,
+  type: 'jsonp',
+  success: (response) => {
+    console.log(response)
+  },
+  error: (err) => {
+    console.error('Oh no!  Something went wrong: ', err.tostring())
+  },
+  headers: {
+    'Authorization': 'Bearer ' + token,
+  }
+})
